b0699d2e2504da1e113cd231792e8c6a62b42ceb
worldintro <worldintro@mail.ru>
2018-10-31 22:44:34 +0300

    decisiontree

---
+++ decisiontree.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+#X - samples Y-features
+X = np.array([[-1,-1], [-2, -1], [-3, -2], [1,1], [3,2], [2, 2]])
+Y = np.array([1, 1, 1, 2, 2, 2])
+#import model
+from sklearn import tree
+clf = tree.DecisionTreeRegressor()
+#X - samples Y-features
+clf.fit(X, Y)
+#predict values
+print(clf.predict([[-3, -1]]))
+print(clf.predict([[3, 1]]))
